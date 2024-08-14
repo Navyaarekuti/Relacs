667a7ad362953692cebfab06102a4ee8287fa30a
Navyaarekuti <arekutinavya1@gmail.com>
2024-08-14 01:18:03 -0500

    polymorphism basics

--- src/main/java/com/relacs/inheritance/objcreation/Client.java
+++ src/main/java/com/relacs/inheritance/objcreation/Client.java
@@ -16,9 +16,17 @@ public class Client {
       public B() {
           System.out.println("B");
       }
+      public B(String s)
+      {
+          System.out.println("B:" +s);
+      }
   }
     class C extends B{
+//        public C(){
+//            System.out.println("C");
+//        }
         public C(){
+            super("Hello");
             System.out.println("C");
         }
     }
---
+++ src/main/java/com/relacs/polymorphism/Client.java
@@ -0,0 +1,26 @@
+package com.relacs.polymorphism;
+
+public class Client {
+    public static void main(String[] args) {
+        User u = new User();
+        User inst = new Instructor();
+        u.email = "nvya@j";
+        inst.username = "Navya";
+//        Instructor i = new User();  <- Not allowed
+        System.out.println(u.email);
+        System.out.println(u.username);
+
+
+        System.out.println(inst.email);
+        System.out.println(inst.username);
+    }
+}
+
+class User{
+    String username;
+    String email;
+}
+
+class Instructor extends User {
+    String batchName;
+}
