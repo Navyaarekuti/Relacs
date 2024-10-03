3749a3197c5e30317968623886b8cbb7d537c7b9
Navyaarekuti <arekutinavya1@gmail.com>
2024-10-03 11:28:47 -0500

    Numberthread

--- src/main/java/com/relacs/threads/basic/BasicThread.java
+++
@@ -1,9 +0,0 @@
-package com.relacs.threads.basic;
-
-public class BasicThread implements Runnable{
-    @Override
-    public void run()
-    {
-        System.out.println("I running on thread:" + Thread.currentThread().getName());
-    }
-}
