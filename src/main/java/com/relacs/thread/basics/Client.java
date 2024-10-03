7c62461dfdabbb9b6890bed193df69bee4945423
Navyaarekuti <arekutinavya1@gmail.com>
2024-10-03 11:45:29 -0500

    basicsthreads

---
+++ src/main/java/com/relacs/thread/basics/Client.java
@@ -0,0 +1,13 @@
+package com.relacs.thread.basics;
+
+public class Client {
+    public static void main(String[] args) {
+    BasicThread basicThread = new BasicThread();
+    Thread t = new Thread(basicThread);
+//        t.run();  //<- This is not going to create a new thread
+        t.start();
+    Thread t1 = new Thread(basicThread);
+        t1.start();
+        System.out.println(Thread.currentThread().getName());
+}
+}
